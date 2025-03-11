cc3e8b6d1d4a26329fc72b636e3ebbc72a5ac3a7
Dogancan Ozturk <d.ozturk@pharma-integration.it>
2025-03-11 11:04:25 +0100

    o3 flag

--- H264SharpNative/pch.cpp
+++ H264SharpNative/pch.cpp
@@ -35,4 +35,15 @@ cmake -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" 
       -DANDROID_ABI=x86_64 \
       -G Ninja .
 
+
+
+
+
+
+      export ANDROID_NDK="C:/Program Files (x86)/Android/AndroidNDK/android-ndk-r27c"
+export PATH="$ANDROID_NDK/toolchains/llvm/prebuilt/windows-x86_64/bin:$PATH"
+
+cmake -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
+      -DANDROID_ABI=arm64-v8a \
+      -G Ninja .
 */
